50d28d893f12f586e7c8fc3192c307b443875b4b
spchan <shihping.chan@gmail.com>
2019-08-14 13:55:27 +0800

    JS whitespace indent: 2

--- js/models/Data.js
+++ js/models/Data.js
@@ -4,105 +4,105 @@ import Storage from './storage'
 import Todo from './todo'
 
 var Data = {
-    list: [],
-    load: function() {
-	Data.list = Storage.get();
-    },
+  list: [],
+  load: function() {
+    Data.list = Storage.get();
+  },
 
-    visible: function(attrs) {
-	if (!('filter' in attrs)) {
-	    return Data.list;
-	} else {
-	    return Data.list.filter(function(item) {
-		if (attrs.filter === 'completed') {
-		    return item.completed;
-		}
-		else {
-		    return !item.completed;
-		}
-	    });
+  visible: function(attrs) {
+    if (!('filter' in attrs)) {
+      return Data.list;
+    } else {
+      return Data.list.filter(function(item) {
+	if (attrs.filter === 'completed') {
+	  return item.completed;
 	}
-    },
-
-    isEmpty: function() {
-	return Data.list.length == 0;
-    },
-    
-    add: function(e) {
-	var title = e.target.value.trim();
-	if (title) {
-	    Data.list.push(new Todo({title: title}));
-	    Storage.put(Data.list);
+	else {
+	  return !item.completed;
 	}
-	e.target.value = '';
-    },
+      });
+    }
+  },
 
-    complete: function(task) {
-	task.completed = !task.completed;
-	Storage.put(Data.list);
-    },
+  isEmpty: function() {
+    return Data.list.length == 0;
+  },
 
-    edit: function(todo) {
-	todo.previousTitle = todo.title;
-	todo.editing = true;
-    },
+  add: function(e) {
+    var title = e.target.value.trim();
+    if (title) {
+      Data.list.push(new Todo({title: title}));
+      Storage.put(Data.list);
+    }
+    e.target.value = '';
+  },
 
-    doneEditing: function(todo, index) {
-	if (!todo.editing) {
-	    return;
-	}
+  complete: function(task) {
+    task.completed = !task.completed;
+    Storage.put(Data.list);
+  },
 
-	todo.editing = false;
-	todo.title = todo.title.trim();
+  edit: function(todo) {
+    todo.previousTitle = todo.title;
+    todo.editing = true;
+  },
 
-	if (!todo.title) {
-	    Data.list.splice(index, 1);
-	}
-	Storage.put(Data.list);
-    },
+  doneEditing: function(todo, index) {
+    if (!todo.editing) {
+      return;
+    }
 
-    cancelEditing: function(todo) {
-	todo.title = todo.previousTitle;
-	todo.editing = false;
-    },
+    todo.editing = false;
+    todo.title = todo.title.trim();
 
-    clearTitle: function(e) {
-	e.target.value = '';
-    },
+    if (!todo.title) {
+      Data.list.splice(index, 1);
+    }
+    Storage.put(Data.list);
+  },
 
-    remove: function(index) {
-	Data.list.splice(index, 1);
-	Storage.put(Data.list);
-    },
+  cancelEditing: function(todo) {
+    todo.title = todo.previousTitle;
+    todo.editing = false;
+  },
 
-    clearCompleted: function() {
-	var list = Data.list;
+  clearTitle: function(e) {
+    e.target.value = '';
+  },
 
-	for (var i = list.length - 1; i >= 0; i--) {
-	    if (list[i].completed) {
-		list.splice(i, 1);
-	    }
-	}
-	Storage.put(list);
-    },
+  remove: function(index) {
+    Data.list.splice(index, 1);
+    Storage.put(Data.list);
+  },
 
-    amountCompleted: function() {
-	return Data.list.filter(e => e.completed).length
-    },
+  clearCompleted: function() {
+    var list = Data.list;
 
-    amountActive: function() {
-	return Data.list.filter(e => !e.completed).length
-    },
+    for (var i = list.length - 1; i >= 0; i--) {
+      if (list[i].completed) {
+	list.splice(i, 1);
+      }
+    }
+    Storage.put(list);
+  },
 
-    allCompleted: function() {
-	return Data.list.filter(e => !e.completed).length == 0;
-    },
+  amountCompleted: function() {
+    return Data.list.filter(e => e.completed).length
+  },
 
-    completeAll: function() {
-	var allCompleted = Data.allCompleted();
-	Data.list.map(item => {item.completed = !allCompleted;});
-	Storage.put(Data.list);
-    },
+  amountActive: function() {
+    return Data.list.filter(e => !e.completed).length
+  },
+
+  allCompleted: function() {
+    return Data.list.filter(e => !e.completed).length == 0;
+  },
+
+  completeAll: function() {
+    var allCompleted = Data.allCompleted();
+    Data.list.map(item => {item.completed = !allCompleted;});
+    Storage.put(Data.list);
+  },
 };
 
 export default Data;
